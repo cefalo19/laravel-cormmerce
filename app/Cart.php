a3e6c2e36cf0f212ec7dc57e20afdf18479faa59
Victor Hugo <cefalo19@gmail.com>
2015-07-15 15:17:37 -0300

    Add method quantity to Cart

---
+++ app/Cart.php
@@ -0,0 +1,58 @@
+<?php
+namespace CodeCommerce;
+
+
+class Cart {
+
+    private $items;
+
+    public function __construct()
+    {
+        $this->items = [];
+    }
+
+    public function add($id, $name, $price)
+    {
+        $this->items += [
+            $id => [
+                'qtd' => isset($this->items[$id]['qtd']) ? $this->items[$id]['qtd']++ : 1,
+                'price' => $price,
+                'name' => $name
+            ]
+        ];
+
+        return $this->items;
+    }
+
+    public function remove($id)
+    {
+        unset($this->items[$id]);
+    }
+
+    public function all()
+    {
+        return $this->items;
+    }
+
+    public function getTotal()
+    {
+        $total = 0;
+
+        foreach($this->items as $item) {
+            $total += $item['qtd'] * $item['price'];
+        }
+
+        return $total;
+    }
+
+    public function quantity($id, $qtd)
+    {
+        ($qtd == 1) ? $this->items[$id]['qtd']++ : $this->items[$id]['qtd']--;
+
+        if ($this->items[$id]['qtd'] <= 0)
+            $this->remove($id);
+
+        return $this->items;
+    }
+
+} 
